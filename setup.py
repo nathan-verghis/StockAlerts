4cc60a229c80ba521712fac6c38f087ec4702e3a
Nathan Verghis <nverghis@uoguelph.ca>
2021-08-19 23:00:00 -0400

    Uncommented needed section

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ from datetime import datetime
 def get_tickers(stocks_filename):
     with open(stocks_filename, 'r') as f:
         stocks = json.load(f)
-    # while True:
-    #     now = datetime.now()
-    #     market_close = datetime(datetime.now().year, datetime.now().month, datetime.now().day, 17, 0, 0, 0)
-    #     market_open = datetime(datetime.now().year, datetime.now().month, datetime.now().day, 19, 0, 0, 0)
-    #     if now > market_open and now < market_close:
-    for stock_name in stocks.keys():
-        if stock_name != "User":
-            lower = stocks[stock_name]["lower_bound"]
-            upper = stocks[stock_name]["upper_bound"]
-            alerts.tracker(stock_name, upper, lower, stocks["User"])
+    while True:
+        now = datetime.now()
+        market_close = datetime(datetime.now().year, datetime.now().month, datetime.now().day, 17, 0, 0, 0)
+        market_open = datetime(datetime.now().year, datetime.now().month, datetime.now().day, 19, 0, 0, 0)
+        if now > market_open and now < market_close:
+            for stock_name in stocks.keys():
+                if stock_name != "User":
+                    lower = stocks[stock_name]["lower_bound"]
+                    upper = stocks[stock_name]["upper_bound"]
+                    alerts.tracker(stock_name, upper, lower, stocks["User"])
 
 
 
